a4f2ca179a640b6eebb5d482be5439eb80f622e7
Robert Sanders <robert@zeevex.com>
2011-12-29 15:59:36 -0500

    handle missing word-stat entries in CSV writer

--- lib/correct_horse_battery_staple/writer/csv.rb
+++ lib/correct_horse_battery_staple/writer/csv.rb
@@ -8,8 +8,8 @@ class CorrectHorseBatteryStaple::Writer::Csv < CorrectHorseBatteryStaple::Writer
     puts "index,rank,word,frequency,percentile,distance,probability,distance_probability"
     corpus.each_with_index do |w, index|
       puts sprintf("%d,%d,\"%s\",%d,%.4f,%.6f,%.8f,%.8f\n",
-        index, w.rank, w.word, w.frequency || 0,
-        w.percentile || 0, w.distance || 0, w.probability || 0, w.distance_probability || 0)
+        index || -1, w.rank || -1, w.word, w.frequency || -1,
+        w.percentile || -1, w.distance || -1, w.probability || -1, w.distance_probability || -1)
     end
   end
 end
