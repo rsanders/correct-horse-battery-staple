bf2f53c4fc29205a69b8a956dbac145d3935e0ff
Robert Sanders <robert@zeevex.com>
2012-01-02 06:35:23 -0500

    fix @id_key final value after converting to Redis

--- lib/correct_horse_battery_staple/writer/redis.rb
+++ lib/correct_horse_battery_staple/writer/redis.rb
@@ -14,6 +14,7 @@ class CorrectHorseBatteryStaple::Writer::Redis < CorrectHorseBatteryStaple::Writ
     create_database
     open_database
 
+    # this is faster and atomic (for some ops), but it doesn't allow RMW cycles
     db.multi do
       save_entries(corpus)
       save_stats(corpus.stats)
@@ -32,7 +33,7 @@ class CorrectHorseBatteryStaple::Writer::Redis < CorrectHorseBatteryStaple::Writ
     corpus.each_with_index do |w, index|
       add_word(w, index)
     end
-    db.set(@id_key, size+1)
+    db.set(@id_key, size-1)
   ensure
   end
 
