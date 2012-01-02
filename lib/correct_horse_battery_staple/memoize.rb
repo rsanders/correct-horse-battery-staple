2d8c750be7be2019bef19fb9f3dcf65bce1d0e05
Robert Sanders <robert@zeevex.com>
2012-01-01 22:24:25 -0500

    fix memoize to use actual method name (oops)

--- lib/correct_horse_battery_staple/memoize.rb
+++ lib/correct_horse_battery_staple/memoize.rb
@@ -10,12 +10,13 @@ module CorrectHorseBatteryStaple::Memoize
       alias_method old_method, method
       define_method method do |*args, &block|
         @_memoize_cache ||= {}
+        methcache = (@_memoize_cache[method] ||= {})
         if block
           raise ArgumentError, "You cannot call a memoized method with a block! #{method}"
         end
-        value = @_memoize_cache.fetch(args, miss_object)
+        value = methcache.fetch(args, miss_object)
         if value === miss_object
-          value = @_memoize_cache[args] = send(old_method, *args)
+          value = methcache[args] = send(old_method, *args)
         end
         value
       end
